ceb1c052939d72cba0a5f58ea266959e3c813816
OliBomby <olivierschipper@gmail.com>
2022-10-11 13:17:52 +0200

    added basic EditSongSelect

---
+++ osu.Game/Screens/Select/EditSongSelect.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Allocation;
+using osu.Framework.Input.Events;
+using osu.Game.Overlays;
+using osu.Game.Users;
+using osuTK.Input;
+
+namespace osu.Game.Screens.Select
+{
+    public class EditSongSelect : SongSelect
+    {
+        [Resolved(CanBeNull = true)]
+        private INotificationOverlay? notifications { get; set; }
+
+        public override bool AllowExternalScreenChange => true;
+
+        protected override UserActivity InitialActivity => new UserActivity.ChoosingBeatmap();
+
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            //BeatmapOptions.AddButton(@"Edit", @"beatmap", FontAwesome.Solid.PencilAlt, colours.Yellow, () => Edit());
+        }
+
+        protected override BeatmapDetailArea CreateBeatmapDetailArea()
+        {
+            return new EditBeatmapDetailArea();
+        }
+
+        protected override bool OnKeyDown(KeyDownEvent e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                case Key.KeypadEnter:
+                    // this is a special hard-coded case; we can't rely on OnPressed (of SongSelect) as GlobalActionContainer is
+                    // matching with exact modifier consideration (so Ctrl+Enter would be ignored).
+                    FinaliseSelection();
+                    return true;
+            }
+
+            return base.OnKeyDown(e);
+        }
+
+        protected override bool OnStart()
+        {
+            Edit();
+            return true;
+        }
+    }
+}
